02ee5ceadcad8eb55502e7ba914c1a26b093efdc
Alex Carroll <alex.carroll@target.com>
2014-11-16 20:30:16 -0600

    quick css fix

--- js/application.js
+++ js/application.js
@@ -1,15 +1,15 @@
 $(document).ready(function(){
   if($('#projectNav').length !=0){
     $('#nav').css('background', '#ffffff');
-    $(window).scroll(function(){ 
+    $(window).scroll(function(){
       if ( $(window).scrollTop() >= $("#nav").offset().top ){
           $("#nav").css({
             position : "fixed",
             top : "0"
           });
-    
+
           $("#pushDiv").css("height",$("#nav").height()+10);
-      } 
+      }
       if ( $(window).scrollTop() <= $("#pushDiv").offset().top ){
           $("#nav").css({
             position : "inherit",
@@ -17,31 +17,16 @@ $(document).ready(function(){
           });
           $("#pushDiv").css("height","0");
       }
-      if ( $(window).scrollTop() + $("#nav").height() + 115 > $('#diablomode').offset().top && $(window).scrollTop() + $("#nav").height() + 115 < ($('#diablomode').offset().top + $('#diablomode').height()) ){
+      /*if ( $(window).scrollTop() + $("#nav").height() + 115 > $('#diablomode').offset().top && $(window).scrollTop() + $("#nav").height() + 115 < ($('#diablomode').offset().top + $('#diablomode').height()) ){
         $(".dmLink").css("color","#F16522");
       }else {
         $(".dmLink").css("color","#000000");
-      } 
+      }
       if ( $(window).scrollTop() + $("#nav").height() + 115 > $('#andrewschroeder').offset().top && $(window).scrollTop() + $("#nav").height() + 115 < ($('#andrewschroeder').offset().top + $('#andrewschroeder').height()) ){
         $(".asLink").css("color","#F16522");
       }else {
         $(".asLink").css("color","#000000");
-      }
-      if ( $(window).scrollTop() + $("#nav").height() + 115 > $('#twenty20').offset().top && $(window).scrollTop() + $("#nav").height() + 115 < ($('#twenty20').offset().top + $('#twenty20').height()) ){
-        $(".twentyLink").css("color","#F16522");
-      }else {
-        $(".twentyLink").css("color","#000000");
-      }
-      if ( $(window).scrollTop() + $("#nav").height() + 115 > $('#slurp').offset().top && $(window).scrollTop() + $("#nav").height() + 115 < ($('#slurp').offset().top + $('#slurp').height()) ){
-        $(".slurpLink").css("color","#F16522");
-      }else {
-        $(".slurpLink").css("color","#000000");
-      }
-      if ( $(window).scrollTop() + $("#nav").height() + 115 > $('#learningwco').offset().top && $(window).scrollTop() + $("#nav").height() + 115 < ($('#learningwco').offset().top + $('#learningwco').height()) ){
-        $(".wcoLink").css("color","#F16522");
-      }else {
-        $(".wcoLink").css("color","#000000");
-      } 
+    }*/
     });
     function goToByScroll(id){
       $('html,body').animate({scrollTop: $(id).offset().top - 97},'slow');
@@ -55,12 +40,12 @@ $(document).ready(function(){
   if($('.message').length != 0){
     goToByScroll($('.message'));
   }
-  
+
   if ($("link[rel='stylesheet']").attr("href") == "/css/style.css"){
     var imageset = [],
       i=0,
       images;
-    
+
     $.each($('.code-images'), function(iter, el) {
       images = [];
       for(i=0; i < $(el).find('img').length; i++) {
@@ -68,41 +53,41 @@ $(document).ready(function(){
       }
       imageset.push(images);
     })
-    
+
     $(".slider-controls").css("display","block");
     $(".code-images a img").click(function(e) {
       goToByScroll($(this).closest("article"));
       $(this).closest('article').find(".projectImage").attr('src', $(this).attr('src'));
-    
+
       return false;
     });
-    
+
     var match_against, current, next;
-    
+
     function previous(current, images) {
       if(current == 0) {
         next = images.length - 1;
       } else {
         next = current - 1;
       }
-      
+
       return images[next];
     }
-    
+
     function next_img(current, images) {
       if(current == images.length - 1) {
         next = 0;
       } else {
         next = current + 1;
       }
-      
+
       return images[next];
     }
-    
+
     function offset(state, position, current_el) {
       match_against = imageset[position];
       current = $.inArray($(current_el).attr('src'), match_against);
-      
+
       if(state == 'prev') {
         new_el = previous(current, match_against);
       } else {
@@ -116,5 +101,5 @@ $(document).ready(function(){
     })
   }
 
-  
+
 });
