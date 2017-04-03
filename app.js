9fa8888e06006281bcae150e17c8d3bb39b83dfe
Alex Carroll <alexcarroll.mn@gmail.com>
2017-04-03 09:31:49 -0700

    heroku deploy stuff

--- app.js
+++ app.js
@@ -10,13 +10,13 @@ var app = express();
 ******************************/
 
 app.use([express.static("views"), express.static("static")]);
-
+app.set('port', (process.env.PORT || 5000));
 app.get ("/", function (req, res) {
     res.render ("/index.html");
 });
 /*****************************
 	  Start the server
 ******************************/
-app.listen(3000, function () {
-  console.log('Server starting on :3000');
+app.listen(app.get('port'), function () {
+  console.log('Server starting');
 });
